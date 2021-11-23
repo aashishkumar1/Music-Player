459798cf44fed0150187666f02a06fff048d39ed
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-23 22:19:56 +0530

    Update
    Now songs are palying.
    play,pause,prev,next and progress bar are working.

--- Js/script.js
+++ Js/script.js
@@ -17,7 +17,7 @@ let isMusicPaused = true; // by default music will be paused
 
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
-    //playingSong();
+    playingSong();
 });
 
 function loadMusic(indexNumb){
@@ -25,5 +25,82 @@ function loadMusic(indexNumb){
     musicArtist.innerHTML = allMusic[indexNumb - 1].artist;
     musicImg.src= `./Images/${allMusic[indexNumb - 1].img}.jpg`;
     mainAudio.src = `./Songs/${allMusic[indexNumb - 1].src}.mp3`;
-
 }
+//play music function
+function playMusic(){
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+//pause music function
+function pauseMusic(){
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+//prev music function
+function prevMusic(){
+    musicIndex--;
+    if(musicIndex < 1)
+    {
+        musicIndex = allMusic.length;
+    }
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
+//Next music function
+function nextMusic(){
+    musicIndex++;
+    if(musicIndex > 6)
+    {
+        musicIndex = 1;
+    }
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
+
+playPauseBtn.addEventListener("click",() => {
+    const isMusicPlay = wrapper.classList.contains("paused");
+    isMusicPlay ? pauseMusic() : playMusic();
+    playingSong();
+});
+prevBtn.addEventListener("click",() => {
+    prevMusic();
+});
+nextBtn.addEventListener("click",() => {
+    nextMusic();
+});
+
+mainAudio.addEventListener("timeupdate",(e) => {
+const currentTime = e.target.currentTime;
+const duration = e.target.duration;
+
+let progressWidth = (currentTime/duration)*100;
+progressBar.style.width = `${progressWidth}%`;
+
+let musicCurrentTime = wrapper.querySelector(".current-timer");
+let musicDuration = wrapper.querySelector(".duration-timer");
+
+mainAudio.addEventListener("loadeddata",() => {
+    let mainAdDuration = mainAudio.duration;
+    let totalMin = Math.floor(mainAdDuration / 60);
+    let totalSec = Math.floor(mainAdDuration % 60);
+    if(totalSec < 10)
+    {
+        totalSec = `0${totalSec}`;
+    }
+    musicDuration.innerText = `${totalMin}:${totalSec}`;
+});
+
+let currentMin = Math.floor(currentTime / 60);
+let currentSec = Math.floor(currentTime % 60);
+if(currentSec < 10)
+    {
+        currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+
+});
+
